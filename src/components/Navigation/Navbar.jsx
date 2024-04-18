a6b653938fb0d6d9717ecc790980faf5baa7c51c
lamba01 <jamesspaul987@gmail.com>
2024-04-18 15:51:29 +0100

    i designed the navigation bar across all device size

---
+++ src/components/Navigation/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './navbar.css'
+import logo from '../../assets/logo.svg'
+
+function Navigation() {
+
+  return (
+    <div> <nav className="navbar">
+    <div className="navbar-container containerr">
+      <input type="checkbox" name="" id="" />
+      <div className="hamburger-lines">
+        <span className="line line1"></span>
+        <span className="line line2"></span>
+        <span className="line line3"></span>
+      </div>
+      <ul className ="menu-items">
+       <li>Pricing</li>
+       <li>Product</li>
+       <li>About us</li> 
+       <li>Career</li> 
+       <li>Community</li>
+      </ul>
+       <div className="logo-container"><img src={logo} alt='logo'/></div>
+       <button className='nav-button'>Get Started</button>
+    </div>
+  </nav></div>
+  )
+}
+
+export default Navigation
